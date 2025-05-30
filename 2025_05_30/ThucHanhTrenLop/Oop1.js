f2d5c51b33cab7a4f24465430c5c109ee185438a
Phạm Đức <p.mduc.ptit@gmail.com>
2025-05-30 21:25:06 +0700

    Up bt

---
+++ 2025_05_30/ThucHanhTrenLop/Oop1.js
@@ -0,0 +1,9 @@
+student = {id:"B24DCCC070", name:"Pham Minh Duc", Group:"D24CQCC04-B"}  
+document.writeln(student.id + '<br>' + student.name + "<br>" + student.Group);
+
+// c2:
+// student=new Object();  
+//    student.id="B24DCCC070";  
+//    student.name="Pham Minh Duc";  
+//    student.Group='D24CQCC04-B';  
+//    document.writeln(student.id+" "+student.name+" "+student.Group);  
