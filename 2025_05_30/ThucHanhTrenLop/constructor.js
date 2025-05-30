5da5266985e2ec489df0713f1abb1ec8008c5784
Phạm Đức <p.mduc.ptit@gmail.com>
2025-05-30 22:02:37 +0700

    Up Bt trên lớp

---
+++ 2025_05_30/ThucHanhTrenLop/constructor.js
@@ -0,0 +1,11 @@
+class student{
+    name;
+    id;
+    group;
+    constructor() {  
+        document.writeln("Anime - My heart");
+    }  
+    Konichiwa(){
+        document.writeln("Konichiwa"+ this.name);
+    }
+}
