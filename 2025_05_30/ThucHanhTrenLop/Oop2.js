fac92c796024b56552633c7cb30e763dfb37447a
Phạm Đức <p.mduc.ptit@gmail.com>
2025-05-30 21:29:27 +0700

    Up Bt

---
+++ 2025_05_30/ThucHanhTrenLop/Oop2.js
@@ -0,0 +1,7 @@
+function student(maSV, Ten, Lop){  
+this.id = maSV;  
+this.name = Ten;  
+this.group = Lop;  
+}  
+d= new student("B24DCCC070","Pham Minh Duc", "D24CQCC04-B");  
+document.writeln(d.id +" "+ d.name +" "+ d.group); 
