af3c0b4f491373ce63044822efa04f152b8d6138
Phạm Đức <p.mduc.ptit@gmail.com>
2025-06-20 08:12:43 +0700

    Up Bth 4

---
+++ 2025_06_20/Bài thực hành 4/index.js
@@ -0,0 +1,20 @@
+const slides = [
+    "https://images3.alphacoders.com/716/thumb-350-716428.webp",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqBcukeGlF7jNwfTxXVV-Vv7zW-4CRNcq7wA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDsa1mtOh3e4TUMnIHe8rHx-xAM7hjnleMAA&s"
+];
+const slide = document.querySelector('.slide img');
+s = 0;
+
+function showSlide(n) {
+    s = (n + slides.length) % slides.length;
+    slide.src = slides[s];
+}
+
+document.getElementById('prev').addEventListener('click', function click(){
+    showSlide(s - 1);
+});
+document.getElementById('next').addEventListener('click', function click(){
+    showSlide(s + 1);
+});
+showSlide(s)
