a4c57d44b51fe56948a78295e6e31ae0a63d862b
Phạm Đức <p.mduc.ptit@gmail.com>
2025-06-23 19:28:18 +0700

    add Js in genre list

---
+++ 2025_07_11/Bài tập lớn/AnimeList.js
@@ -0,0 +1,25 @@
+const animeList = [
+  {
+    img: "https://i1.sndcdn.com/artworks-000102584894-8rr4kl-t240x240.jpg",
+    alt: "Lời nói dối tháng tư",
+    link: "https://animevietsub.lol/phim/thang-tu-la-loi-noi-doi-cua-em-i1-a2432/",
+    name: "Shigatsu kimi no uso (Lời nói dối tháng tư)",
+    genres: "romcom, school life"
+  },
+  {
+    img: "https://i1.sndcdn.com/artworks-000102584894-8rr4kl-t240x240.jpg",
+    alt: "Lời nói dối tháng tư",
+    link: "https://animevietsub.lol/phim/thang-tu-la-loi-noi-doi-cua-em-i1-a2432/",
+    name: "Shigatsu kimi no uso (Lời nói dối tháng tư)",
+    genres: "romcom, school life"
+  },
+  {
+    img: "https://i1.sndcdn.com/artworks-000102584894-8rr4kl-t240x240.jpg",
+    alt: "Lời nói dối tháng tư",
+    link: "https://animevietsub.lol/phim/thang-tu-la-loi-noi-doi-cua-em-i1-a2432/",
+    name: "Shigatsu kimi no uso (Lời nói dối tháng tư)",
+    genres: "romcom, school life"
+  }
+
+];
+
---
+++ 2025_07_11/Bài tập lớn/GenreList.js
@@ -0,0 +1,10 @@
+const genreList = [
+  { genre: "romcom", name: "Romcom" },
+  { genre: "school life", name: "School life" },
+  { genre: "slice of life", name: "Slice of Life" },
+  { genre: "shounen", name: "Shounen" },
+  { genre: "isekai", name: "Isekai" },
+  { genre: "mystery", name: "Mystery" },
+  { genre: "action", name: "Action" },
+  { genre: "harem", name: "Harem" }
+];
