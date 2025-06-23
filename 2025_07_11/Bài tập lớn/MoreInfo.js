ffd800a9da0285c6c6e76dff599253ceb8c3e3b6
Phạm Đức <p.mduc.ptit@gmail.com>
2025-06-23 19:42:12 +0700

    add js in Cộng đồng anime

---
+++ 2025_07_11/Bài tập lớn/MoreInfo.js
@@ -0,0 +1,33 @@
+// Cộng đồng, fanpage
+const fanpage_groupList = [
+  { name: "Anime - My Heart", link: "https://www.facebook.com/share/g/18xNoFDLvE/" },
+  { name: "Hako - Cộng đồng độc giả Light Novel Việt Nam", link: "https://www.facebook.com/share/g/1DLjz7xbJm/" },
+  { name: "Động Hảo Ngọt 2.0", link: "https://www.facebook.com/share/g/16XhijQFpB/" },
+  { name: "Động Hảo Ngọt 3.0", link: "https://www.facebook.com/share/g/1CKSVunD91/" },
+  { name: "Hako.vn", link: "https://www.facebook.com/hako.page" },
+  { name: "Đứa biết nhiều nhất về Light Novel ở Việt Nam", link: "https://www.facebook.com/NaviRanobe" },
+  { name: "Đứa biết nhiều thứ 2 về Light Novel chuyên đi reply comment xin name", link: "https://www.facebook.com/TheMeoki/" },
+  { name: "Zennomi", link: "https://www.facebook.com/Zennomi" },
+  { name: "Tùng Bơ", link: "https://www.facebook.com/Mc.TungBo" },
+  { name: "Anime Funny Times", link: "https://www.facebook.com/Anime.Funny.Times" },
+  { name: "...--Sẽ update thêm--...", link: "" }
+];
+
+// Các Anime Festival quanh Hà Nội và cộng đồng Cosplay
+const festivalList = [
+  {
+    name: "HIMAWARI ARTIST FIESTA",
+    link: "https://www.facebook.com/groups/1981734985637068/user/61567643504074/",
+    date: "Chủ nhật: 25/05/2025",
+    place: "Pont de Long Biên, Hoài Đức, Hà Nội.",
+    map: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.4197743745995!2d105.71979857530077!3d21.015883380630235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abc378bbcc2d%3A0x9145b902d586ffba!2zUGjhu5EgxJBpIELhu5kgUG9udCBEZSBMb25nIEJpZW4!5e0!3m2!1svi!2s!4v1747845290741!5m2!1svi!2s" width="250" height="100" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+  },
+  {
+    name: "𝐀𝐧𝐢𝐦𝐞 𝐂𝐨𝐥𝐥𝐞𝐜𝐭𝐢𝐨𝐧 𝐓𝐨𝐮𝐫 𝐬𝐞𝐚𝐬𝐨𝐧 𝟏𝟓: 𝑰𝒏𝒕𝒆𝒓𝒏𝒂𝒕𝒊𝒐𝒏𝒂𝒍 𝑪𝒉𝒊𝒍𝒅𝒓𝒆𝒏'𝒔 𝑫𝒂𝒚",
+    link: "https://www.facebook.com/share/1EYDoimML3/",
+    date: "Chủ nhật: 01/06/2025",
+    place: "Vincom Center Phạm Ngọc Thạch",
+    map: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.659022409151!2d105.82938107530039!3d21.006301080637424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad95816c146f%3A0x2490ed9e600b1009!2zVmluY29tIENlbnRlciBQaOG6oW0gTmfhu41jIFRo4bqhY2g!5e0!3m2!1svi!2s!4v1747845554660!5m2!1svi!2s" width="250" height="100" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+  },
+  // ...Thêm các festival khác tương tự...
+];
