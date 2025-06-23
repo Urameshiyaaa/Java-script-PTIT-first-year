557841f11eae84b92e145f110c3c6fefe4afb117
Phạm Đức <p.mduc.ptit@gmail.com>
2025-06-23 19:38:30 +0700

    add js in manga, ln list

---
+++ 2025_07_11/Bài tập lớn/LightnovelList.js
@@ -0,0 +1,22 @@
+const lnList = [
+  {
+    img: "https://nami.news/wordpress/wp-content/uploads/2025/04/Class-de-2-ban-Me-ni-Kawaii-Onna-no-Ko-to-Tomodachi-ni-Natta-Novel-Cover-7.5.jpg",
+    alt: "Làm bạn với cô nàng dễ thương nhì lớp",
+    name: "Kurasu de Nibanme ni Kawaii onna no ko to Tomodachi ni natta",
+    genres: "romcom, school life",
+    more: `<img src="https://images2.imgbox.com/1a/58/SvwbYhkv_o.jpg" alt="Anime Illustration">
+           <h3>クラスで２番目に可愛い女の子と友だちになった<br>Làm bạn với cô nàng dễ thương nhì lớp</h3>
+           <div style="margin: 4px 0;"><span class="genre">Romcom</span><span class="genre">School life</span><span class="genre">Adapted to manga</span><span class="genre">Web novel</span></div>
+           <p><u>Tác giả</u>: Takata<br><u>Họa sĩ</u>: Azuri Hyuuga<br><u>Tình trạng</u>: Đang tiến hành<br><u>Tóm tắt</u>:<br>
+           Một câu chuyện hài lãng mạn ngọt ngào bắt nguồn từ mối quan hệ bạn bè bí mật!
+
+Tôi là Maehara Maki, đã không có bạn bè, hay thậm chí là người quen khi bước vào cao trung, cuối cùng bây giờ cũng đã có bạn đi chơi ở ngoài trường. Đó là một cô gái.
+
+Cô ấy tên là Asanagi. Đám con trai hay gọi cô ấy là “Cô gái đáng yêu thứ hai trong lớp”.
+
+Cứ vào thứ sáu, cô ấy lại từ chối lời mời của bạn thân Amami, và đến nhà tôi chơi.
+
+Vừa chơi game, xem phim, đọc manga, vừa uống Coca, ăn Junk Food được ship đến như Hamburger hay Pizza. Đó là khoảng thời gian ăn chơi của tôi và Asanagi-san, nhưng nó cũng là một bí mật rất quan trọng.<br>
+           <u>Đọc tại</u>:<a href="https://docln.sbs/truyen/9193-kurasu-de-nibanme-ni-kawaii-onna-no-ko-to-tomodachi-ni-natta" target="_blank" style="text-decoration: none; color: #a1d1fb;"> KuraNika</a></p>`
+  },
+];
---
+++ 2025_07_11/Bài tập lớn/MangaList.js
@@ -0,0 +1,24 @@
+const mangaList = [
+  {
+    img: "https://sakarychan.wordpress.com/wp-content/uploads/2023/07/class-de-2-banme-ni-kawaii-onnanoko-to-tomodachi-ni-natta.jpg",
+    alt: "Gái xinh nhì",
+    link: "https://truyenqqgo.com/truyen-tranh/lam-ban-voi-co-gai-dang-yeu-thu-hai-lop-12902",
+    name: "Làm bạn với cô nàng đáng yêu thứ nhì lớp",
+    genres: "romcom, school life"
+  },
+  {
+    img: "https://product.hstatic.net/200000287623/product/changbanggiavananglanhlung_2_manga_bia_1_2e950b78817847fcb07db2b8b652d40d_master.png",
+    alt: "Chàng băng giá, nàng lạnh lùng",
+    link: "https://truyenqqgo.com/truyen-tranh/chang-bang-gia-va-nang-lanh-lung-7076",
+    name: "Chàng băng giá, nàng lạnh lùng",
+    genres: "romcom"
+  },
+  {
+    img: "https://storage-ct.lrclib.net/file/cuutruyen/uploads/manga/2369/cover/processed-3a1c00b03ca0de03666536af6cf6be2e.jpg",
+    alt: "Satou Koharu",
+    link: "https://truyenqqgo.com/truyen-tranh/shiotaiou-no-sato-san-ga-ore-ni-dake-amai-10307",
+    name: "Shiotaiou No Sato-San Ga Ore Ni Dake Amai",
+    genres: "romcom, school life"
+  },
+  // ...Thêm các bộ manga khác tương tự...
+];
