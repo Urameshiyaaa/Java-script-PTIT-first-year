8cce16142e1accecb465d60d81098613c8a3f169
Phạm Đức <p.mduc.ptit@gmail.com>
2025-06-27 10:54:16 +0700

    fix js searchbar

--- 2025_07_11/Bài tập lớn/Search.js
+++ 2025_07_11/Bài tập lớn/Search.js
@@ -1,16 +1,22 @@
-document.getElementById('searchInput').addEventListener('input', function(e) {
-    const searchTerm = e.target.value.toLowerCase();
-    const sections = [
-        {class: 'love-grid', items: '.love-item'},
-        {class: 'anime-grid', items: '.anime-item'},
-        {class: 'ln-section .scroll', items: '.ln-item'}
-    ];
+x = document.getElementById('searchBar')
+x.addEventListener('input', function(a){
+    const searchWord = a.target.value.toLowerCase()
 
-    sections.forEach(section => {
-        const items = document.querySelectorAll(`.${section.class} ${section.items}`);
-        items.forEach(item => {
-            const title = item.querySelector('h3').textContent.toLowerCase();
-            item.style.display = title.includes(searchTerm) ? 'flex' : 'none';
+    const sections = [
+        {class: 'anime-grid', items: '.anime-item'},
+        {class: 'scroll', items: '.ln-item'},
+        {class: 'manga-grid', items: '.anime-item'}
+    ]
+    sections.forEach( section => {
+        const b = document.querySelectorAll(`.${section.class} ${section.items}`)
+        b.forEach( item => {
+            const title = item.querySelector('h3').textContent.toLowerCase()
+            if (title.includes(searchWord)) {
+                item.style.display = 'flex'
+            }
+            else{
+                item.style.display = 'none'
+            }
         });
     });
 });
